0fb45b33800723a6eef2b2dcdeea1b72a577ce44
William Raendchen <william.raendchen@logiway.de>
2019-05-02 11:31:01 +0200

    removed HoloHelper::cached function because exactly the same can be done with Laravel's Cache:remember

--- src/HoloHelper.php
+++ src/HoloHelper.php
@@ -84,18 +84,4 @@ class HoloHelper
 		if (strpos($haystack, $needle) !== false) return true;
 		return false;
 	}
-
-	public static function cached(string $key, \Closure $function, int $duration) {
-		if (Cache::has($key)) {
-			return Cache::get($key);
-		}
-
-		// execute the function to generate our result
-		$result = $function();
-
-		// store the result
-		Cache::put($key, $result, $duration);
-		
-		return $result;
-	}
 }
